aea510775b6ea11703fb4bd19be009b08de01422
moralmunky <christopher@moralmunky.com>
2022-04-07 22:59:10 -0400

    Update update.sh
    Removed spaces

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ rm master.zip
 wget "https://github.com/moralmunky/SmartRent-MQTT-Bridge/archive/refs/heads/master.zip"
 unzip -o master.zip
 cp smartrent.env SmartRent-MQTT-Bridge/smartrent.env
-cd SmartRent-MQTT-Bridge                      
+cd SmartRent-MQTT-Bridge
 docker build . -t smartrent-mqtt-bridge
