a829500668f3ced73a634b03ab88fce32eb346ec
moralmunky <christopher@moralmunky.com>
2022-04-07 21:57:45 -0400

    Added payload print to bridge

--- python/smartrent-bridge.py
+++ python/smartrent-bridge.py
@@ -149,9 +149,11 @@ class SmartRentBridge:
             if attribute == "locked":
                 mqtt_client.publish(MQTT_TOPIC_PREFIX + '/' + devices[device_id][1] + '/status', payload=value, qos=1, retain=True)
                 print(MQTT_TOPIC_PREFIX + '/' + devices[device_id][1] + '/status')
+                print("Payload: " + value)
             if attribute == "notifications":
                 mqtt_client.publish(MQTT_TOPIC_PREFIX + '/' + devices[device_id][1] + '/detail', payload=value, qos=1, retain=True)
                 print(MQTT_TOPIC_PREFIX + '/' + devices[device_id][1] + '/detail')
+                print("Payload: " + value)
         print(message)
         return
 
